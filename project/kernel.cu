d3ca604fab8634ec365be3359e73555de2e372d7
Attila Zsiros <attila.zsiros@outlook.sk>
2021-01-06 15:31:27 +0100

    atomic add

--- project/kernel.cu
+++ project/kernel.cu
@@ -1,11 +1,11 @@
 #define BLOCK_SIZE_X 32
 #define BLOCK_SIZE_Y 8
-#define REDUCTION_BLOCK_SIZE 256
 
 __device__ bool check_neighbours_border(const int* const in, const int n, const int x, const int y, const int threshold)
 {	
 	int infected_neighbours = 0;
 	for (int dy = -1; dy <= 1; ++dy) {
+		#pragma unroll
 		for (int dx = -1; dx <= 1; ++dx) {
 			// check bounds
 			if ((dx == 0 && dy == 0) || (x + dx < 0) || (x + dx >= n) || (y + dy < 0) || (y + dy >= n))
@@ -23,22 +23,25 @@ __device__ bool check_neighbours_inner(const int* const in, const int n, const i
 {	
 	int infected_neighbours = 0;
 
-	for (int dy = -1; dy <= 1; ++dy) {
-		int row = (y + dy) * n;
-		infected_neighbours += in[row + (x - 1)] > 0 ? 1 : 0;
-		infected_neighbours += in[row + x] > 0 ? 1 : 0;
-		infected_neighbours += in[row + (x + 1)] > 0 ? 1 : 0;
-	}
+	int row = (y - 1) * n;
+	infected_neighbours += in[row + (x - 1)] > 0 ? 1 : 0;
+	infected_neighbours += in[row + x] > 0 ? 1 : 0;
+	infected_neighbours += in[row + (x + 1)] > 0 ? 1 : 0;
+
+	row += n;
+	infected_neighbours += in[row + (x - 1)] > 0 ? 1 : 0;
+	infected_neighbours += in[row + (x + 1)] > 0 ? 1 : 0;
+
+	row += n;
+	infected_neighbours += in[row + (x - 1)] > 0 ? 1 : 0;
+	infected_neighbours += in[row + x] > 0 ? 1 : 0;
+	infected_neighbours += in[row + (x + 1)] > 0 ? 1 : 0;
 
 	return infected_neighbours > threshold;
 }
 
-__global__ void make_iteration(const int* const in, int* const out, const int* const contacts, int* const iter_block_infections, const int iter, const int n)
+__global__ void make_iteration(const int* const in, int* const out, const int* const contacts, int* const infections, const int iter, const int n)
 {
-	__shared__ int shared_iter_block_infections[BLOCK_SIZE_X * BLOCK_SIZE_Y];
-	const int idx_local = threadIdx.y * blockDim.x + threadIdx.x;
-	shared_iter_block_infections[idx_local] = 0;
-
 	const unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
 	const unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
 
@@ -58,7 +61,7 @@ __global__ void make_iteration(const int* const in, int* const out, const int* c
 
 		if (infected) {
 			house_out = 10;
-			++shared_iter_block_infections[idx_local];
+			atomicAdd(&infections[iter], 1);
 		}
 	} else if (house_in > 0) { // infected
 		house_out = house_in - 1 == 0 ? -30 : house_in - 1;
@@ -66,83 +69,29 @@ __global__ void make_iteration(const int* const in, int* const out, const int* c
 		house_out = house_in + 1;
 	}
 
-	__syncthreads();
-
-	// --- reduction ---
-
-	// sum and save the total number of new infections in this iteration per block
-	int infections = shared_iter_block_infections[idx_local];
-	for (unsigned int offset = (BLOCK_SIZE_X * BLOCK_SIZE_Y) >> 1; offset > 32; offset >>= 1) {
-		if (idx_local < offset)
-			shared_iter_block_infections[idx_local] = infections = infections + shared_iter_block_infections[idx_local + offset];
-
-		__syncthreads();
-	}
-
-	// reduce last warp
-	if (idx_local < 32) {
-		infections += shared_iter_block_infections[idx_local + 32];
-
-		for (unsigned int offset = warpSize >> 1; offset > 0; offset >>= 1)
-			infections += __shfl_down_sync(0xffffffff, infections, offset);
-	}
-
-	if (idx_local == 0) {
-		const unsigned int iter_block_idx = (iter * gridDim.x * gridDim.y) + (blockIdx.y * gridDim.x + blockIdx.x);
-		iter_block_infections[iter_block_idx] = infections;
-	}
-
 	out[idx_global] = house_out;
-}
-
-/*
-   For each iteration, sums infections per block to compute the number of new infections per iteration.
-*/
-__global__ void sum_infections(int* const infections, const int* const iter_block_infections, const int iters, const dim3 grid_size)
-{
-	const int iter = blockIdx.x;
-	const int iter_idx = iter * grid_size.x * grid_size.y;
-	
-	if (threadIdx.x == 0) {
-		int iter_sum = 0;
-
-		for (int i = 0; i < grid_size.x * grid_size.y; ++i)
-			iter_sum += iter_block_infections[iter_idx + i];
-
-		infections[iter] = iter_sum;
-	}
 }
 
 void solveGPU(const int* const contacts, int* const city, int* const infections, const int n, const int iters)
 {
-	dim3 grid_size = dim3(ceil(n / (float) BLOCK_SIZE_X), ceil(n / (float) BLOCK_SIZE_Y));
-
 	int *in = city;
 	int *out;
 
-	// 3D array storing infections per block per iteration
-	int *iter_block_infections; // [iters][grid_size][grid_size] 
-
-	if ((cudaMalloc((void**)&out, n * n * sizeof(int)) != cudaSuccess)
-			|| (cudaMalloc((void**)&iter_block_infections, iters * grid_size.x * grid_size.y * sizeof(int)) != cudaSuccess)) {
+	if (cudaMalloc((void**)&out, n * n * sizeof(int)) != cudaSuccess) {
 		fprintf(stderr, "cudaMalloc failed\n");
 		return;
 	}
 
 	dim3 threads_per_block = dim3(BLOCK_SIZE_X, BLOCK_SIZE_Y);
+	dim3 grid_size = dim3(ceil(n / (float) BLOCK_SIZE_X), ceil(n / (float) BLOCK_SIZE_Y));
 	dim3 blocks_per_grid = dim3(grid_size.x, grid_size.y);
 	for (int iter = 0; iter < iters; ++iter) {
-		make_iteration<<<blocks_per_grid, threads_per_block>>>(in, out, contacts, iter_block_infections, iter, n);
+		make_iteration<<<blocks_per_grid, threads_per_block>>>(in, out, contacts, infections, iter, n);
 
 		int *tmp = in;
 		in = out;
 		out = tmp;
 	}
-
-	// sum infections per iteration
-	threads_per_block = REDUCTION_BLOCK_SIZE;
-	blocks_per_grid = iters;
-	sum_infections<<<blocks_per_grid, threads_per_block>>>(infections, iter_block_infections, iters, grid_size);
 
 	if (in != city) {
 		cudaMemcpy(city, in, n*n*sizeof(int), cudaMemcpyDeviceToDevice);
@@ -150,6 +99,4 @@ void solveGPU(const int* const contacts, int* const city, int* const infections,
 	} else {
 		cudaFree(out);
 	}
-
-	cudaFree(iter_block_infections);
 }
